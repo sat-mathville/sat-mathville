5a4b38c207f61cd60fdfed7c51ab428371074796
Bronwyn Harris <bronwyn.harris@Bronwyns-MacBook-Air.local>
2018-08-07 16:10:47 -0400

    added character model and satDate to user model

---
+++ server/db/models/character.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Character = db.define('character', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false  
+  }
+})
+
+module.exports = Character
